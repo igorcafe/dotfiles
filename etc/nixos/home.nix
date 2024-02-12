1fa9b44ee99a729bd7600d8e2daba6a891cb4dd8
Igor Melo <igoracmelo@protonmail.com>
2024-02-12 14:50:50 -0300

    home-manager: vscode + git + nixpkgs-fmt

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -1,18 +1,13 @@
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{ inputs, lib, config, pkgs, ... }: {
   nixpkgs = {
 
     overlays = [
       (final: prev: {
-        postman = prev.postman.overrideAttrs(old: rec {
+        postman = prev.postman.overrideAttrs (old: rec {
           version = "20230716100528";
           src = final.fetchurl {
-            url = "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
+            url =
+              "https://web.archive.org/web/${version}/https://dl.pstmn.io/download/latest/linux_64";
             sha256 = "sha256-svk60K4pZh0qRdx9+5OUTu0xgGXMhqvQTGTcmqBOMq8=";
 
             name = "${old.pname}-${version}.tar.gz";
@@ -23,7 +18,12 @@
 
     config = {
       allowUnfree = true;
-      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "steam" "steam-original" "steam-run" ];
+      allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "steam"
+          "steam-original"
+          "steam-run"
+        ];
     };
   };
 
@@ -40,7 +40,6 @@
       google-chrome
       telegram-desktop
       gnome.cheese
-      vscode
       kcalc
       signal-desktop
       gcc
@@ -78,17 +77,116 @@
       xonotic
       jq
       postman
+      nixpkgs-fmt
     ];
+  };
+
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    extensions = with pkgs.vscode-extensions; [ vscodevim.vim ];
+    keybindings = [
+      {
+        key = "ctrl+shift+c";
+        command = "editor.action.clipboardCopyAction";
+        when = "textInputFocus";
+      }
+      {
+        key = "alt+d";
+        command = "editor.action.addSelectionToNextFindMatch";
+        when = "editorFocus";
+      }
+      {
+        key = "ctrl+d";
+        command = "-editor.action.addSelectionToNextFindMatch";
+        when = "editorFocus";
+      }
+      {
+        key = "alt+r";
+        command = "workbench.action.openRecent";
+      }
+      {
+        key = "ctrl+r";
+        command = "-workbench.action.openRecent";
+      }
+      {
+        key = "ctrl+shift+v";
+        command = "-markdown.showPreview";
+        when = "!notebookEditorFocused && editorLangId == 'markdown'";
+      }
+      {
+        key = "alt+f";
+        command = "actions.find";
+        when = "editorFocus || editorIsOpen";
+      }
+      {
+        key = "ctrl+f";
+        command = "-actions.find";
+        when = "editorFocus || editorIsOpen";
+      }
+      {
+        key = "ctrl+k o";
+        command = "workbench.action.closeOtherEditors";
+      }
+      {
+        key = "ctrl+k o";
+        command = "-workbench.action.files.showOpenedFileInNewWindow";
+        when = "emptyWorkspaceSupport";
+      }
+      {
+        key = "ctrl+k ctrl+alt+s";
+        command = "git.stageSelectedRanges";
+        when = "!operationInProgress";
+      }
+      {
+        key = "ctrl+k ctrl+alt+s";
+        command = "-git.stageSelectedRanges";
+        when = "isInDiffEditor && !operationInProgress";
+      }
+    ];
+
+    userSettings = {
+      # themes
+      "workbench.colorTheme" = "Default Light Modern";
+      "workbench.iconTheme" = "material-icon-theme";
+
+      # vim
+      "vim.handleKeys" = {
+        "<C-n>" = false;
+        "<C-p>" = false;
+        "<C-b>" = false;
+        "<C-k>" = false;
+      };
+      "vim.normalModeKeyBindings" = [
+        {
+          "before" = [ "g" "i" ];
+          "commands" = [ "editor.action.goToImplementation" ];
+        }
+      ];
+      "vim.smartRelativeLine" = true;
+
+      # go
+      "go.lintTool" = "golangci-lint";
+      "go.testTimeout" = "5s";
+
+      #nix
+      "nix.enableLanguageServer" = true;
+      "[nix]" = {
+        "editor.defaultFormatter" = "jnoortheen.nix-ide";
+      };
+
+      "files.trimTrailingWhitespace" = true;
+      # "html.autoClosingTags" = false;
+      "editor.minimap.enabled" = false;
+      "git.suggestSmartCommit" = false;
+      "window.zoomLevel" = 1;
+    };
   };
 
   programs.git = {
     enable = true;
     extraConfig = {
-      url = {
-        "git@github.com:" = {
-          insteadOf = "https://github.com/";
-        };
-      };
+      url = { "git@github.com:" = { insteadOf = "https://github.com/"; }; };
     };
   };
 
@@ -108,22 +206,34 @@
     zplug = {
       enable = true;
       plugins = [
-        { name = "plugins/git"; tags = [ from:oh-my-zsh ]; }
-        { name = "plugins/colored-man-pages"; tags = [ from:oh-my-zsh ]; }
-        { name = "plugins/sudo"; tags = [ from:oh-my-zsh ]; }
-        { name = "themes/robbyrussell"; tags = [ from:oh-my-zsh ]; }
+        {
+          name = "plugins/git";
+          tags = [ "from:oh-my-zsh" ];
+        }
+        {
+          name = "plugins/colored-man-pages";
+          tags = [ "from:oh-my-zsh" ];
+        }
+        {
+          name = "plugins/sudo";
+          tags = [ "from:oh-my-zsh" ];
+        }
+        {
+          name = "themes/robbyrussell";
+          tags = [ "from:oh-my-zsh" ];
+        }
         { name = "zsh-users/zsh-autosuggestions"; }
         { name = "zsh-users/zsh-syntax-highlighting"; }
       ];
     };
     initExtra = ''
-    bindkey "^[[1;5C" forward-word
-    bindkey "^[[1;5D" backward-word
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
 
-    gotemp () {
-      d=/tmp/gotemp-$RANDOM
-      mkdir $d && cd $d && echo "package main\n\nfunc main() {\n\t\n}\n" > main.go
-    }
+      gotemp () {
+        d=/tmp/gotemp-$RANDOM
+        mkdir $d && cd $d && echo "package main\n\nfunc main() {\n\t\n}\n" > main.go
+      }
     '';
   };
 }
