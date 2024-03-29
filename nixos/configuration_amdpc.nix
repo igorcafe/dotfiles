c98dd1e6dc3b01452d0a3bbeb1321ea86b38a888
Igor Melo <igoracmelo@protonmail.com>
2024-03-29 14:01:53 -0300

    nixos: hardware configuration per machine

--- nixos/configuration_amdpc.nix
+++ nixos/configuration_amdpc.nix
@@ -2,13 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     # Include the results of the hardware scan.
     ./configuration_common.nix
-    ./hardware-configuration.nix
+    ./hardware-configuration_amdpc.nix
   ];
 
 
