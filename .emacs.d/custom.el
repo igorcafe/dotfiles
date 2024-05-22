698492e10ee9141c5a9baed7fa4044af0512393c
Igor Melo <igor.melo@tino.com.br>
2024-05-22 11:38:53 -0300

    emacs: early init, vertico-posframe, TODOs

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(package-selected-packages
-   '(eldoc-box zen-mode writeroom-mode which-key vterm vertico use-package undo-tree treemacs sudoku restart-emacs rainbow-identifiers pomodoro pomidor pdf-tools pacmacs org-pomodoro org-bullets orderless nix-mode neotree multiple-cursors magit lsp-mode key-chord ivy go-mode evil-multiedit evil-mc evil-collection empv doom-themes doom-modeline diff-hl corfu command-log-mode all-the-icons)))
+   '(vertico-posframe writeroom-mode which-key undo-tree treemacs restart-emacs protobuf-mode pdf-tools org-bullets orderless nix-mode neotree magit key-chord go-mode evil-mc evil-collection eldoc-box doom-themes doom-modeline diff-hl corfu all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
