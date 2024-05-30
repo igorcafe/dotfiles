8335a131370c43312a74225e186c4291d7b392f5
Igor Melo <igoracmelo@protonmail.com>
2024-05-30 00:23:07 -0300

    emacs

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" default))
  '(package-selected-packages
-   '(markdown-mode yaml-mode writeroom-mode which-key web-server vterm vertico-posframe use-package undo-tree treemacs telega sudoku spinner restart-emacs pomodoro pomidor perspective pdf-tools pacmacs org-roam-ui org-pomodoro org-bullets orderless nix-ts-mode nix-mode neotree multiple-cursors magit key-chord ivy go-mode evil-multiedit evil-mc evil-collection esup empv ement eldoc-box doom-themes doom-modeline diff-hl corfu consult command-log-mode blamer all-the-icons)))
+   '(telega yaml-mode writeroom-mode which-key web-server vterm vertico-posframe use-package undo-tree treemacs sudoku spinner restart-emacs perspective pdf-tools pacmacs org-roam-ui org-bullets orderless nix-ts-mode nix-mode neotree multiple-cursors markdown-mode key-chord ivy go-mode evil-multiedit evil-mc evil-collection esup empv ement eldoc-box doom-themes doom-modeline diff-hl corfu consult command-log-mode blamer all-the-icons alert)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
