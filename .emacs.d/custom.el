d13165dda59ffe708bdec3d761f1f69902e8f3ce
Igor Café <igorcafe@protonmail.com>
2024-06-17 12:44:53 -0300

    emacs: wip dap mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yaml-mode xterm-color writeroom-mode which-key web-server vterm vertico-posframe use-package undo-tree treemacs sudoku spinner restart-emacs rainbow-identifiers perspective pdf-tools pacmacs org-roam-ui org-bullets org-auto-tangle orderless nix-ts-mode nix-mode neotree multiple-cursors markdown-mode marginalia magit key-chord ivy go-mode focus exwm evil-multiedit evil-mc evil-collection esup empv ement elfeed eldoc-box doom-themes doom-modeline diff-hl corfu consult command-log-mode blamer all-the-icons-dired alert)))
+   '(yaml-mode writeroom-mode which-key vterm vertico-posframe undo-tree toc-org terraform-mode telega restart-emacs request protobuf-mode perspective pdf-tools org-roam-ui org-present-remote org-bullets org-auto-tangle orderless oauth2 nix-mode neotree marginalia magit key-chord go-mode focus evil-mc evil-collection esup emojify ement elfeed eldoc-box doom-themes doom-modeline diff-hl dap-mode corfu circe blamer benchmark-init all-the-icons-dired alert adaptive-wrap)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -184,6 +184,8 @@
   (global-set-key (kbd "C-SPC") #'completion-at-point)
   (global-corfu-mode 1))
 
+(use-package dap-mode)
+
 (recentf-mode 1)
 (setq recentf-max-menu-items 100)
 (setq recentf-max-saved-items 100)
