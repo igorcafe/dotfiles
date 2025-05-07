3ae4b672e97fbadfb4f8c4fd25bb264b8ca5d7be
Igor Cafe <igorcafe@protonmail.com>
2025-05-07 14:30:05 -0300

    emacs: custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,16 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(aider all-the-icons blamer company consult dape diff-hl
+   '(aider all-the-icons auto-dark blamer company consult dape diff-hl
            dired-preview dired-subtree doom-modeline doom-themes eat
            eldoc-box elfeed envrc erc-hl-nicks eshell-prompt-extras
-           esup evil-collection evil-surround go-tag gptel key-chord
-           marginalia mpv nix-mode nov olivetti orderless org-appear
-           org-cliplink org-download org-drill org-fragtog
-           org-mpv-notes org-music org-noter org-roam-ql org-roam-ui
+           esup evil-collection evil-numbers evil-surround go-tag
+           gptel key-chord marginalia monkeytype mpv nix-mode nov
+           olivetti orderless org-appear org-cliplink org-download
+           org-drill org-fragtog org-modern org-mpv-notes org-music
+           org-noter org-pomodoro org-present org-roam-ql org-roam-ui
            pdf-tools perspective pinentry restclient sideline-flymake
-           toc-org typescript-mode vertico vue-mode yaml-mode
-           yasnippet yeetube)))
+           telega toc-org typescript-mode vertico vterm web-mode
+           yaml-mode yasnippet yeetube))
+ '(package-vc-selected-packages
+   '((org-music :vc-backend Git :url
+                "https://github.com/debanjum/org-music.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
