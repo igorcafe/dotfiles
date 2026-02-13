8683ef781fc27e5ea7d86c9defc13ed2571596fa
Igor Cafe <igorcafe@protonmail.com>
2026-02-13 12:24:03 -0300

    nixos: nixpkgs: Fri Jan 23 12:18:02 2026 -> Wed Feb 11 20:21:07 2026

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS system configuration(s)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { nixpkgs, ... }: {
