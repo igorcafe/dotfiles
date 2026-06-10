2b242b03caae9433f6fb7776682af5751a8ea5f8
Igor Cafe <igorcafe@protonmail.com>
2026-06-10 12:13:36 -0300

    emacs: custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,18 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons blamer company consult dape diff-hl dired-preview
-                   dired-subtree doom-modeline doom-themes eat
-                   eldoc-box elfeed-org erc-hl-nicks
-                   eshell-prompt-extras esup evil-collection
-                   evil-surround general go-tag gptel haskell-mode
-                   key-chord magit marginalia markdown-mode mpv
-                   nix-mode nov olivetti orderless org-appear
-                   org-cliplink org-download org-drill org-fragtog
-                   org-mpv-notes org-music org-noter org-roam-ql
-                   org-roam-ui pdf-tools perspective-tabs pinentry
-                   popper reformatter restclient typescript-mode
-                   vertico web-mode yaml-mode yasnippet yeetube)))
+   '(all-the-icons-dired blamer company consult dape diff-hl
+                         dired-preview dired-subtree doom-modeline
+                         doom-themes eat eldoc-box elfeed-org
+                         erc-hl-nicks eshell-prompt-extras esup
+                         evil-collection evil-surround general go-tag
+                         gptel haskell-mode key-chord lua-mode magit
+                         marginalia markdown-mode mpv nix-mode nov
+                         olivetti orderless org-appear org-cliplink
+                         org-download org-drill org-fragtog
+                         org-mpv-notes org-music org-noter org-roam-ql
+                         org-roam-ui pdf-tools perspective-tabs
+                         pinentry popper reformatter restclient
+                         scopeline typescript-mode vertico web-mode
+                         yaml-mode yasnippet yeetube)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
