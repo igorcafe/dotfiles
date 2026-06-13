de18f4b6638df62191ac55fe3fdca288bf47a59f
Igor Cafe <igorcafe@protonmail.com>
2026-06-13 11:25:01 -0300

    emacs: current custom.el before starting over

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,15 +9,19 @@
                          doom-themes eat eldoc-box elfeed-org
                          erc-hl-nicks eshell-prompt-extras esup
                          evil-collection evil-surround general go-tag
-                         gptel haskell-mode key-chord lua-mode magit
-                         marginalia markdown-mode mpv nix-mode nov
-                         olivetti orderless org-appear org-cliplink
-                         org-download org-drill org-fragtog
-                         org-mpv-notes org-music org-noter org-roam-ql
-                         org-roam-ui pdf-tools perspective-tabs
-                         pinentry popper reformatter restclient
-                         scopeline typescript-mode vertico web-mode
-                         yaml-mode yasnippet yeetube)))
+                         gptel haskell-mode indent-guide key-chord
+                         lua-mode magit marginalia markdown-mode mpv
+                         nix-mode nov olivetti orderless org-appear
+                         org-cliplink org-download org-drill
+                         org-fragtog org-mpv-notes org-music org-noter
+                         org-roam-ql org-roam-ui pdf-tools
+                         perspective-tabs pinentry popper reformatter
+                         restclient scopeline telega typescript-mode
+                         vertico web-mode window-stool yaml-mode
+                         yasnippet yeetube))
+ '(package-vc-selected-packages
+   '((window-stool :vc-backend Git :url
+                   "https://github.com/JasZhe/window-stool"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
